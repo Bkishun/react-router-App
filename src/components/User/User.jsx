97e07ee2bb72550db966d7f852f997a9fa20e289
Balkishun Maurya <bkishun21072001@gmail.com>
2024-03-20 01:20:19 +0530

    github component added

---
+++ src/components/User/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function User() {
+    const {userid} = useParams()
+  return (
+    <div className='bg-green-700 text-black text-3xl p-4'>
+      User :{userid}
+    </div>
+  )
+}
+
+export default User
